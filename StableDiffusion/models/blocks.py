f99f8fc902568c3047514fc31e83a3d0eeb31aea
vinayak-sharan <vinayak.kumar@casablanca.ai>
2024-05-06 09:27:56 +0200

    vqvae: re-implement vqvae

--- StableDiffusion/models/blocks.py
+++ StableDiffusion/models/blocks.py
@@ -42,7 +42,7 @@ class DownBlock(nn.Module):
     def forward(self, x, t_emb):
         out = x
         out = self.resnet_in(out)
-        if self.t_emb:
+        if t_emb is not None:
             t_emb = self.t_emb(t_emb).unsqueeze(-1).unsqueeze(-1)
             out = out + t_emb
 
@@ -83,16 +83,17 @@ class MidBlock(nn.Module):
     def __init__(self, in_channels, out_channels, t_emb, num_heads, num_layers=1):
         super().__init__()
         self.num_layers = num_layers
-        self.num_heads = num_heads
+        self.num_heads = 4 if num_heads == 0 else num_heads
         self.first_resnet_block_in = nn.Sequential(
             nn.GroupNorm(8, in_channels),
             nn.SiLU(),
             nn.Conv2d(in_channels, out_channels, 3, 1, 1)
         )
-        self.first_t_emb = nn.Sequential(
-            nn.SiLU(),
-            nn.Linear(t_emb, out_channels)
-        )
+        if t_emb:
+            self.first_t_emb = nn.Sequential(
+                nn.SiLU(),
+                nn.Linear(t_emb, out_channels)
+            )
         self.first_resnet_block_out = nn.Sequential(
             nn.GroupNorm(8, out_channels),
             nn.SiLU(),
@@ -102,17 +103,18 @@ class MidBlock(nn.Module):
 
         self.attn_norm = nn.ModuleList([nn.GroupNorm(8, out_channels) for _ in range(num_layers)])
         self.self_attention = nn.ModuleList(
-            [nn.MultiheadAttention(out_channels, num_heads, batch_first=True) for _ in range(num_layers)])
+            [nn.MultiheadAttention(out_channels, self.num_heads, batch_first=True) for _ in range(num_layers)])
 
         self.second_resnet_block_in = nn.ModuleList([nn.Sequential(
             nn.GroupNorm(8, out_channels),
             nn.SiLU(),
             nn.Conv2d(out_channels, out_channels, 3, 1, 1)
         ) for _ in range(num_layers)])
-        self.second_t_emb = nn.ModuleList([nn.Sequential(
-            nn.SiLU(),
-            nn.Linear(t_emb, out_channels)
-        ) for _ in range(num_layers)])
+        if t_emb:
+            self.second_t_emb = nn.ModuleList([nn.Sequential(
+                nn.SiLU(),
+                nn.Linear(t_emb, out_channels)
+            ) for _ in range(num_layers)])
         self.second_resnet_block_out = nn.ModuleList([nn.Sequential(
             nn.GroupNorm(8, out_channels),
             nn.SiLU(),
@@ -123,7 +125,8 @@ class MidBlock(nn.Module):
     def forward(self, x, t_emb):
         out = x
         out = self.first_resnet_block_in(out)
-        out = out + self.first_t_emb(t_emb).unsqueeze(-1).unsqueeze(-1)
+        if t_emb is not None:
+            out = out + self.first_t_emb(t_emb).unsqueeze(-1).unsqueeze(-1)
         out = out + self.first_resnet_block_out(out)
         out = out + self.first_residual(x)
 
@@ -138,7 +141,8 @@ class MidBlock(nn.Module):
             resnet_in = att_out
 
             out = self.second_resnet_block_in[layer](att_out)
-            out = out + self.second_t_emb[layer](t_emb).unsqueeze(-1).unsqueeze(-1)
+            if t_emb is not None:
+                out = out + self.second_t_emb[layer](t_emb).unsqueeze(-1).unsqueeze(-1)
             out = self.second_resnet_block_out[layer](out)
             out = out + self.second_residual[layer](resnet_in)
 
@@ -175,7 +179,7 @@ class UpBlock(nn.Module):
         resnet_in = out
         out = self.resnet_in(out)
 
-        if self.t_emb is not None:
+        if t_emb is not None:
             t_emb = self.t_emb(t_emb).unsqueeze(-1).unsqueeze(-1)
             out = out + t_emb
 
@@ -195,22 +199,27 @@ class UpBlock(nn.Module):
 
 
 class UpBlocks(nn.Module):
-    def __init__(self, in_channels, out_channels, num_heads, t_emb, num_layers, is_up_sample):
+    def __init__(self, in_channels, out_channels, num_heads, t_emb, num_layers, is_up_sample, is_concat):
         super().__init__()
         self.is_up_sample = is_up_sample
         self.upblocks = nn.ModuleList(
             [UpBlock(in_channels if i == 0 else out_channels, out_channels, num_heads, t_emb) for i in
              range(num_layers)])
 
-        self.up_sample = nn.ConvTranspose2d(in_channels // 2, in_channels // 2, 4, 2, 1)
+        if is_concat:
+            self.up_sample = nn.ConvTranspose2d(in_channels // 2, in_channels // 2, 4, 2, 1)
+        else:
+            self.up_sample = nn.ConvTranspose2d(in_channels, in_channels, 4, 2, 1)
 
-    def forward(self, x, x_down, t_emb):
+
+
+    def forward(self, x, x_down=None, t_emb=None):
         out = x
         if self.is_up_sample:
             out = self.up_sample(out)
         if x_down is not None:
             out = torch.cat([x_down, out], dim=1)
         for layer in self.upblocks:
-            out = layer(out, x_down, t_emb)
+            out = layer(out, t_emb)
         return out
 
---
+++ StableDiffusion/models/vqvae.py
@@ -0,0 +1,160 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from StableDiffusion.models.blocks import DownBlocks, UpBlocks, MidBlock
+import yaml
+
+from collections import OrderedDict
+
+
+class Encoder(nn.Module):
+    def __init__(self, im_channels, channels, t_emb=0, is_down_sample=[True], num_heads=0, num_sub_layers=1,
+                 mid_layers=[256, 256]):
+        super().__init__()
+        self.conv_in = nn.Conv2d(im_channels, channels[0], 3, 1, 1, )
+
+        self.downs = nn.ModuleList([
+            DownBlocks(channels[i], channels[i + 1], t_emb, is_down_sample[i], num_heads, num_sub_layers) for i in
+            range(len(channels) - 1)
+        ])
+
+        self.mids = nn.ModuleList([MidBlock(mid_layers[i], mid_layers[i + 1], t_emb, num_heads, num_sub_layers) for i in
+                                   range(len(mid_layers) - 1)])
+
+    def forward(self, x, t_emb=None):
+        out = x
+        out = self.conv_in(out)
+
+        for layer in self.downs:
+            out = layer(out, t_emb)
+
+        for layer in self.mids:
+            out = layer(out, t_emb)
+
+        return out
+
+
+class Decoder(nn.Module):
+    def __init__(self, im_channels, channels, t_emb=0, is_up_sample=[True], num_heads=0, num_sub_layers=1,
+                 is_concat=False):
+        super().__init__()
+        self.ups = nn.ModuleList(
+            [UpBlocks(channels[i], channels[i + 1], num_heads, t_emb, num_sub_layers, is_up_sample[i], is_concat) for i
+             in
+             range(len(channels) - 1)])
+        self.norm_out = nn.GroupNorm(8, channels[-1])
+        self.decoder_out = nn.Conv2d(channels[-1], im_channels, 3, 1, 1)
+
+    def forward(self, x):
+        out = x
+        for layer in self.ups:
+            out = layer(out)
+        out = self.norm_out(out)
+        out = F.silu(out)
+        out = self.decoder_out(out)
+
+        return out
+
+
+class VQVAE(nn.Module):
+    def __init__(self, **args):
+        super().__init__()
+        args_ = args['VQ-VAE']
+        common_args = args_['common_args']
+        im_channels = common_args.pop('im_channels')
+        self.encoder_args = args_['Encoder']
+        self.encoder_args['im_channels'] = im_channels
+        self.decoder_args = args_['Decoder']
+        self.decoder_args['im_channels'] = im_channels
+
+
+        self.z_channels = common_args.pop('z_channels')
+        self.code_book_size = common_args.pop('code_book_size')
+        self.beta = common_args.pop('beta')
+
+        self.encoder = Encoder(**self.encoder_args)
+        self.pre_quant_conv = nn.Sequential(
+            nn.GroupNorm(8, self.encoder_args['channels'][-1]),
+            nn.Conv2d(self.encoder_args['channels'][-1], self.z_channels, 3, 1, 1),
+            nn.Conv2d(self.z_channels, self.z_channels, 1, )
+        )
+        self.embeddings = nn.Embedding(self.code_book_size, self.z_channels)
+
+        self.post_quant_conv = nn.Sequential(
+            nn.Conv2d(self.z_channels, self.z_channels, 1, ),
+            nn.Conv2d(self.z_channels, self.encoder_args['mid_layers'][-1], 3, 1, 1),
+        )
+
+        self.decoder = Decoder(**self.decoder_args)
+
+    def forward(self): ...
+
+    def loss(self, input_):
+        feat = self.encoder(input_)
+        quant_out, quant_loss = self.quantize(feat)
+
+        reconstruction = self.decoder(quant_out)
+
+        # reconstruction loss
+        # recon_loss = F.mse_loss(input_, reconstruction, reduction='none').view(input_.shape[0], -1).sum().mean()
+        recon_loss = F.mse_loss(input_, reconstruction)
+        loss = recon_loss + quant_loss
+
+        result = OrderedDict(loss=loss, quanized_loss=quant_loss)
+
+        return result
+
+    def quantize(self, feat):
+        # self.embeddings = nn.Embedding(512, 3)
+        quant_in = feat.permute(0, 2, 3, 1)  # reshaped to b, h, w, c
+        b, h, w, c = quant_in.shape
+        quant_in = quant_in.view(b, -1, c)
+
+        # calculate distance between quant_in and embeddings,
+        # dist between (B, H*W, C) and (B, K, C) -> (B, H*W, K)
+        dist = torch.cdist(quant_in, self.embeddings.weight.unsqueeze(0).repeat(b, 1, 1))
+
+        # get indices of minimum distances from each rows
+        min_indices = torch.argmin(dist, dim=-1)
+
+        # select the nearest codebook vectors for sending it to decoder
+        quant_out = torch.index_select(self.embeddings.weight, dim=0, index=min_indices.view(-1))
+
+        quantize_loss = self.quantize_loss(quant_in, quant_out)
+
+        quant_in = quant_in.reshape(b * h * w, c)
+
+        # stop gradients to make sure gradient is propagated back to encoder
+        # during fwd prop, the decoder takes quant_out as input but gradient is copied back to encoder during backprop
+        quant_out = quant_in + (quant_out - quant_in).detach()
+
+        quant_out = quant_out.reshape(b, h, w, c)
+        quant_out = quant_out.permute(0, 3, 1, 2)
+
+        return quant_out, quantize_loss
+
+    def quantize_loss(self, quant_in, quant_out):
+        # quant_in: b, h*w, c, quant_out: b*h*w, c
+        quant_in = quant_in.reshape(-1, quant_in.shape[-1])
+
+        # commitment loss: quant_in should be as close as to the quant_out
+        # encoder should commit to codebook vectors, so that encoder avoids fluctuating between different codebook
+        commitment_loss = ((quant_out.detach() - quant_in) ** 2).mean()
+
+        # codebook loss: codebook should be as close as to the input
+        # if codebooks vectors are considered as centroid of embeddings from encoder
+        # then centroid should be as close to quant_in
+        codebook_loss = ((quant_in.detach() - quant_out) ** 2).mean()
+
+        quantize_loss = commitment_loss + self.beta * codebook_loss
+
+        return quantize_loss
+
+
+with open('configs/vqvae.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+config
+
+x = torch.randn(16, 3, 28, 28)
+model = VQVAE(**config)
+out = model.loss(x)
